69f6eeb0ecdce848a121b86f1e5a799d7142f4fc
Bomme -- Lasnier Maxime <maxime.lasnier.01@gmail.Com>
2024-08-07 19:14:11 +0200

    no message

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,20 @@
-import { resolve } from 'path'
-import { defineConfig } from 'vite'
-import rawPlugin from 'vite-raw-plugin'
+import { resolve } from "path";
+import { defineConfig } from "vite";
+import rawPlugin from "vite-raw-plugin";
 
 export default defineConfig({
-	base: "./",
-	plugins: [
-		rawPlugin({
-			fileRegex: /\.html\\?raw$/
-		})
-	],
-	resolve: {
-		alias: {
-			'@': resolve(__dirname, './src'),
-		}
-	}
-})
+  base: "./",
+  plugins: [
+    rawPlugin({
+      fileRegex: /\.html\\?raw$/,
+    }),
+  ],
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "./src"),
+    },
+  },
+  build: {
+    outDir: "dist",
+  },
+});
