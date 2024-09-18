e5b76b5a6ea6dce5bb1bcddd511f7e34cd055002
Bomme -- Lasnier Maxime <maxime.lasnier.01@gmail.Com>
2024-09-18 16:29:55 +0200

    update

--- src/App.ts
+++ src/App.ts
@@ -1,35 +1,34 @@
-import { Component, RouterComponent } from '@/Libs/CustomElementTs'
-import Nootstrap from '@/Libs/Nootstrap'
+import { Component, RouterComponent } from "@/Libs/CustomElementTs";
+import Nootstrap from "@/Libs/Nootstrap";
 
-import { Home, Projects, CV } from './Pages'
+import { Home, Projects, CV } from "./Pages";
 
-import '@/Layouts'
-import './style.sass'
+import "@/Layouts";
+import "./style.sass";
 
-import html from './App.html?raw'
+import html from "./App.html?raw";
 
-const nootstrap = new Nootstrap()
+const nootstrap = new Nootstrap();
 
 @Component({
-	selector: 'app-main',
-	classes: "scroll-snap-proximity",
-	template: html
+  selector: "app-main",
+  classes: "scroll-snap-proximity",
+  template: html,
 })
 class App extends HTMLElement {
+  connected() {
+    this.initRouter();
+  }
 
-	connected() {
-		this.initRouter()
-	}
+  private initRouter(): void {
+    const router = this.querySelector<RouterComponent>("app-router")!;
+    router.mode = "hash";
 
-	private initRouter(): void {
-		const router = this.querySelector<RouterComponent>('app-router')!
-		router.mode = 'hash';
-
-		Array(
-			{ path: '', component: Home },
-			{ path: 'projects', component: Projects },
-			{ path: 'cv', component: CV },
-		).forEach(route => router.addRoute(route))
-		router.listen()
-	}
+    Array(
+      { path: "", component: Home },
+      { path: "projects", component: Projects },
+      { path: "cv", component: CV }
+    ).forEach((route) => router.addRoute(route));
+    router.listen();
+  }
 }
